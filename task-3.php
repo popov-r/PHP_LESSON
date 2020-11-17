35b2c718cb8931f240f575f37e2010a9dad8720e
Krakozabr <popov.ro@rozetka.com.ua>
2020-11-17 16:31:18 +0200

    task 3 done

--- task-3.php
+++ task-3.php
@@ -1,1 +1,14 @@
 <?php
+echo("Введите катеты прямоугольного треугольника через пробел:\n");
+fscanf(STDIN,"%d %d", $a, $b);
+$c=sqrt($a**2+$b**2);
+echo("Гипотенуза : $c\n");
+$s=($a*$b)/2;
+echo("Площадь : $s\n");
+$angle_a=tan($a/$b);
+$angle_a=rad2deg($angle_a);
+settype($angle_a,"integer");
+$angle_b=90-$angle_a;
+echo("Углы равны : 90 $angle_a $angle_b\n");
+$p=$a+$b+$c;
+echo("Периметр : $p\n");
