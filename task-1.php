93c9867de5bdd80c9e2b1e37e9d3f9d2ec5be650
Krakozabr <popov.ro@rozetka.com.ua>
2020-11-18 21:58:25 +0200

    add task 1

---
+++ task-1.php
@@ -0,0 +1,21 @@
+<?php
+$ar1r=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+$b1=18;
+
+function searchInArray($a, $b){
+$arr=$a;
+$searchElement=$b;
+$firstIndex=0;
+$lastIndex=count($arr)-1;
+if ($searchElement>$arr[$lastIndex]){return false;}
+elseif ($arr[$firstIndex]>$searchElement){return false;}
+while($firstIndex<=$lastIndex){
+$midleIndex=($firstIndex+$lastIndex)/2;
+if ($arr[$midleIndex] == $searchElement){return true;}
+elseif ($arr[$midleIndex]<$searchElement){$firstIndex=$midleIndex+1;}
+elseif ($arr[$midleIndex]>$searchElement){$lastIndex=$midleIndex-1;}
+else {return false;}
+}
+}
+$z=searchInArray($ar1r,$b1);
+var_dump($z);
